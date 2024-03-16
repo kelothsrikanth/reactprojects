6a997d2e697f93210ece3eb457135e41a855fb57
npx <srikanthkeloth@lyrostech.com>
2024-03-16 13:26:29 +0530

    commit

--- src/Head.js
+++ src/Head.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Head() {
   return (
-    <div><h1>Srikanth</h1></div>
+    <><h1>Srikanth</h1>
+    <a href="kelothsrikanth.github.io/FaceBook/
+">FaceBook</a>
+<a href="https://kelothsrikanth.github.io/ecomm-react/">ecomm</a>
+    </>
   )
 }
 
